d4e5421b851af250473f838f2ed431dac34f1142
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-01-20 20:23:22 +0100

    20.01 Made Header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Logo from "../../elements/images/logo.png"
+import Logo from "../../elements/images/logo.png";
+import Basket from "../../elements/images/basket=empty.png";
+import style from "./Header.module.css";
 
 export default function Header() {
     return (
-        <div>
-            <img src={Logo} />
+        <div className={style.headerMain}>
+            <img src={Logo} alt="No image" className={style.logo} />
+            <nav className={style.navBar}>
+                <a>Main page</a>
+                <a>Categories</a>
+                <a>All products</a>
+                <a>All sales</a>
+            </nav>
+            <img src={Basket} alt="No image" className={style.basket}/>
         </div>
     );
 }
