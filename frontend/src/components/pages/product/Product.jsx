3e9cef9abf4116147f321410eb875caed1e40f95
VladimirVornikov <vornikov-vladimir@mail.ru>
2024-01-28 22:34:18 +0100

    28.01 Product site

--- frontend/src/components/pages/product/Product.jsx
+++ frontend/src/components/pages/product/Product.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import React, { useEffect } from 'react'
 import style from './Product.module.css'
 import { fetchProduct } from '../../../store/asyncAction/fetchCategories'
+import Button from '../../../elements/buttonCard/CheckOutBtn'
 
 export default function Product() {
     const product = useSelector(store => store.product)
@@ -14,22 +15,32 @@ export default function Product() {
     }, [])
   return (
     <div className={style.productContainer}>
-        <img src={`http://localhost:3333${product.image}`}/>
+        <img src={`http://localhost:3333${product.image}`} className={style.img}/>
         <div>
-            <h3>{product.title}</h3>
+            <h3 className={style.title}>{product.title}</h3>
             <span className={style.priceContainer}>
-                <p>{product.price}</p>
-                <p>{product.discont_price}</p>
+                <p className={style.mainPrice}>{`$${product.discont_price ? product.discont_price : product.price}`}</p>
+                <p className={style.discountPrice}>{product.discont_price ? `$${product.price}` : ''}</p>
+                {product.discont_price ? (
+                        <span className={style.discountBlock}>{discountPercentage(product.price, product.discont_price)}</span>
+                        ) : null}
             </span>
-            <form action="">
-                <button>-</button>
-                <p>1</p>
-                <button>+</button>
-                <button>Add to cart</button>
+            <form className={style.form}>
+                <span className={style.additionSubtraction}>
+                    <button className={style.addSubButtons}>-</button>
+                    <p className={style.price}>1</p>
+                    <button className={style.addSubButtons}>+</button>
+                </span>
+                <Button color={"green"} info={"Add to cart"} width={"316px"} />
             </form>
-            <h3>Description</h3>
-            <p>{product.description}</p>
+            <h3 className={style.description}>Description</h3>
+            <p className={style.infoBlock}>{product.description}</p>
         </div>
     </div>
   )
+}
+
+
+function discountPercentage(price, discount) {
+    return "-" + Math.round(Number(100 * (price - discount) /price))+"%"
 }
